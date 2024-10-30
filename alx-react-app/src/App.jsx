bd2c7b70d56cde7a6cb4a27f0c8c962846950574
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-10-30 13:30:20 +0100

    Updated App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import WelcomeMessage from "./components/WelcomeMessage"
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from "./components/UserProfile";
+
 
 
 function App() {
@@ -21,7 +23,8 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div>   
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <MainContent />
       <WelcomeMessage />
       <h1>Vite + React</h1>
