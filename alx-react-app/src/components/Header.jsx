f105dc07bb33ad047b21a2d04d7866726b9613b4
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-10-30 13:15:35 +0100

    Updated Header.jsx

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,4 +1,8 @@
-export 
-<header>
-  <h1>My Favorite Cities</h1>
-</header>
+export default function Header() {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+}
+
