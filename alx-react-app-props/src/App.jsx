2eacfbb3f57ba61d61c3918486ab2eb7a7378c64
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-07 17:42:29 +0100

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,19 +1,13 @@
-import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile";
+import ProfilePage from "./ProfilePage";
+import { userContext } from "./UserContext";
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <>
-      <Header />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <MainContent />
-      <WelcomeMessage />
-      <Footer />
-    </>
+    <userContext.Provider value={userData}>
+      <ProfilePage />;
+    </userContext.Provider>
   );
 }
 
