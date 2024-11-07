55b0b63dc8354380650f3d71d73b1927499064a8
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-07 16:11:55 +0100

    Updated Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer>
-      <p>© 2023 City Lovers</p>
+      <p style={{ fontWeight: "bolder", padding: "10px", color: "blue" }}>
+        © 2023 City Lovers
+      </p>
     </footer>
   );
 }
