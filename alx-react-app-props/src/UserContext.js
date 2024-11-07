9b5567279a58d45ef53eb4670ce7da14c774bd4e
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-07 17:49:28 +0100

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,3 +1,5 @@
-import React from "react";
+import React from 'react';
 
-export const userContext = React.createContext();
+const UserContext = React.createContext(userData);
+
+export default UserContext;
