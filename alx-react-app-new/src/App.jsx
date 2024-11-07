9a5904255e739f979afe7cc7e8d28ec1fa51eb76
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-07 17:04:19 +0100

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
@@ -9,7 +8,7 @@ import Counter from "./components/Counter";
 
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <>
