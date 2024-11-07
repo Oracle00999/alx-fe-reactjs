d570ded479e3ac759cf756e1b269a5014fa2c938
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-07 16:38:31 +0100

    Add files via upload

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <MainContent />
+      <WelcomeMessage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
