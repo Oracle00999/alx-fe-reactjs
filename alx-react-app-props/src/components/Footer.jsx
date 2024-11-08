af335d59777f01e170ab85e0314a426ce8d11d12
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-08 08:56:56 +0100

    Delete alx-react-app-props/src/components directory

--- alx-react-app-props/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <p style={{ fontWeight: "bolder", padding: "10px", color: "blue" }}>
-        © 2023 City Lovers
-      </p>
-    </footer>
-  );
-}
--- alx-react-app-props/src/components/MainContent.jsx
+++
@@ -1,7 +0,0 @@
-export default function MainContent() {
-  return (
-    <main style={{ fontWeight: "bolder", padding: "10px" }}>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
-  );
-}
