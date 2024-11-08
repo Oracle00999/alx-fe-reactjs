b1724837b634a9137187d2c58b7d3d84ff68ddbf
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-08 08:56:07 +0100

    Add files via upload

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p style={{ fontWeight: "bolder", padding: "10px", color: "blue" }}>
+        © 2023 City Lovers
+      </p>
+    </footer>
+  );
+}
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+export default function MainContent() {
+  return (
+    <main style={{ fontWeight: "bolder", padding: "10px" }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
