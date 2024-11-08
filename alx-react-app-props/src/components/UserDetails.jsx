57efdf42ec59976db8d73f3bac918a0420f0cbd8
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-08 08:57:34 +0100

    Add files via upload

---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,14 @@
+import { userContext } from "./UserContext";
+import React, { useContext } from "react";
+
+function UserDetails() {
+  const userData = useContext(userContext);
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
