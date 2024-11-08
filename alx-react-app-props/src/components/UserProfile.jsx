6e55e4e8c48c861fc7cc16daf91504f842e85fc2
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-08 09:11:06 +0100

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,3 +1,6 @@
+import userContext from "./UserContext";
+import React, { useContext } from "react";
+
 export default function UserProfile(props) {
   return (
     <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
