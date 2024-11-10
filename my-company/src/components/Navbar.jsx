2340a7e3b844b843f8f7a326b4bacd3be2c84e4a
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-10 21:12:28 +0100

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ color: "wheat" }}>
+    <nav
+      style={{
+        color: "wheat",
+        backgroundColor: "blue",
+        display: "block",
+        justifyContent: "center",
+      }}
+    >
       <Link to="home">Home</Link>
       <Link to="about">About</Link>
       <Link to="contact">Contact</Link>
