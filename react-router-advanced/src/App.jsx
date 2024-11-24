747118335184cb469b18cf432d46192c9048232f
Chibuike Anthony Nwazota <111026213+Oracle00999@users.noreply.github.com>
2024-11-24 19:46:40 +0100

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,26 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Login from './components/Login';
+import BlogPost from './components/BlogPost';
+import NotFound from './components/NotFound';
+import UserProfile from './components/UserProfile';
+import ProtectedRoute from './components/ProtectedRoute';
 
-import Home from "./components/Home";
-import About from "./components/About";
-import Profile from "./components/Profile";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
+function App() {
 
-const App = () => {
+  const isAuthenticated = false; // Simulate authentication
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog/:id" element={<BlogPost />} /> 
-        <Route path="/profile/:id" element={<Profile />}>
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Dynamic Blog Route */}
+        <Route path="/profile/*" element={
+           <ProtectedRoute isAuthenticated={isAuthenticated}>
+             <Profile />
+           </ProtectedRoute>
+         }
+          />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/user/:userId" element={<UserProfile />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
